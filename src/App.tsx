d24710fc1a5ea84b5cbe6344f4a5628fc9b12e3c
Vlad Ataman <bearcoder@bearlikecode.com>
2023-10-06 02:56:22 +0300

    Added tailwindcss, sass, axios, formik and yup packages

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="flex w-full">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
